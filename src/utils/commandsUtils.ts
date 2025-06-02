c6e2c880e0e4dc0b94ec1f4ba58616b98e19fcf3
cesxhin <mattia.ceschin@gmail.com>
2025-06-02 09:02:25 +0200

    Fix unique converastion between bot and user for every chat

--- src/utils/commandsUtils.ts
+++ src/utils/commandsUtils.ts
@@ -2,23 +2,27 @@ import _ from "lodash";
 import TelegramBot from "node-telegram-bot-api";
 import { checkMyCommand, commands } from "./botUtils";
 
-const historyCommand = new Map<number, commands>();
+const historyCommand = new Map<string, commands>();
 
 async function command(message: TelegramBot.Message, command: commands, functionExecuteCommand: (message: string) => Promise<void>, functionReadCommand: () => Promise<void>){
     if (!_.isNil(message.from) && !message.from.is_bot){
-        const findCommandFromUser = historyCommand.get(message.from.id);
+        const findCommandFromUser = historyCommand.get(getPrimarykeyCompose(message));
         if (!_.isNil(findCommandFromUser) && findCommandFromUser === command){
 
             if (!_.isNil(message.text)){
                 await functionExecuteCommand(message.text);
-                historyCommand.delete(message.from.id);
+                historyCommand.delete(getPrimarykeyCompose(message));
             }
         } else if (checkMyCommand(message.text, command)){
-            historyCommand.set(message.from.id, command);
+            historyCommand.set(getPrimarykeyCompose(message), command);
 
             await functionReadCommand();
         }
     }
+}
+
+function getPrimarykeyCompose(message: TelegramBot.Message){
+    return `${message.chat.id}-${message.from.id}`;
 }
 
 export default {
