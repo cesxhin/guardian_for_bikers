09c39f6cd122a1cb603831da6253aeb80b179a52
cesxhin <mattia.ceschin@gmail.com>
2025-11-15 10:28:31 +0100

    Improvment script for build and install manual lib "canvas" final step

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM node:24-slim AS builder
+FROM  --platform=$BUILDPLATFORM node:24-slim AS builder
 
 WORKDIR /builder
 
 COPY ./src/ .
 
 RUN apt-get update &&\
-    apt-get install -y build-essential pkg-config librsvg2-dev libcairo2-dev libpango1.0-dev &&\
+    apt-get install -y libpixman-1-0 fonts-dejavu &&\
     npm i &&\
-    ./node_modules/.bin/node-gyp rebuild --release -C ./node_modules/canvas &&\
     npm run build
 
 FROM node:24-slim AS runner
@@ -16,6 +15,6 @@ WORKDIR /app
 
 COPY --from=builder /builder/dist/ .
 
-RUN apt-get update && apt-get install -y libpixman-1-0 fonts-dejavu
+RUN npm i -g canvas
 
 CMD ["node", "./index.js"]
--- src/tsup.config.ts
+++ src/tsup.config.ts
@@ -1,8 +1,6 @@
 import pkg from "./package.json";
 import { defineConfig } from "tsup";
 
-const OUT_DIR = "dist";
-
 export default defineConfig({
     entry: {
         index: "index.ts",
@@ -10,23 +8,17 @@ export default defineConfig({
     splitting: true,
     sourcemap: false,
     clean: true,
-    outDir: OUT_DIR,
+    outDir: "dist",
     format: ["esm"],
     platform: "node",
     env: {
         NODE_ENV: "production"
     },
-    target: ["node22", "es2024"],
-    esbuildOptions: (options) => {
-        if (options.format === "esm"){
-            options.banner = {
-                js: "import { createRequire as cr } from 'module'; const require = cr(import.meta.url);"
-            };
-        }
-    },
+    target: ["node24"],
     external: ["canvas"],
     noExternal: Object.keys(pkg.dependencies).filter((lib) => lib !== "canvas"),
     minify: true,
     treeshake: true,
-    shims: true
+    shims: true,
+    inject: ['cjs-shim.ts']
 });
