b4746af56c32d175c7d16cf4764da3f7c910a118
cesxhin <mattia.ceschin@gmail.com>
2026-06-06 10:15:12 +0200

    Fix missing essentials envs for unit test

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        reporters: process.env.GITHUB_ACTIONS === "true" ? ["tree", "github-actions"] : ["tree"]
+        reporters: process.env.GITHUB_ACTIONS === "true" ? ["tree", "github-actions"] : ["tree"],
+        env: {
+            TOKEN_BOT: "test",
+            USERNAME_BOT: "test"
+        }
     }
 });
